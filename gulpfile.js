a9b5630342769c1c1fbf5467201fbd7b7bf23cf2
thenegativeedge <brunofdsleite@hotmail.com>
2016-11-25 02:49:09 -0200

    Update Gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var bs = browserSync.create();
+var autoprefixer = require('gulp-autoprefixer')
+
+// Inits browserSync server
+gulp.task('browserSync', function(){
+	bs.init({
+		server: "./"
+	});
+});
+
+// Compile files from scss to css
+gulp.task('sass', function(){
+	return gulp.src('./scss/main.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+	.pipe(gulp.dest('./css'))
+	.pipe(bs.stream());
+});
+
+// Watch files changes
+gulp.task('watch', function(){
+	gulp.watch('./scss/*.scss', ['sass']);
+	gulp.watch('./*.html').on('change', bs.reload);
+	gulp.watch('./js/*.js').on('change', bs.reload);
+});
+
+gulp.task('default', ['browserSync', 'watch']);
+
